e365cafa72c92d0734aa9a302e7df265d227c3ca
fakhri01 <habibzade.fakhri@gmail.com>
2024-08-30 00:25:43 +0400

    search button added

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { SearchField } from "./SearchCountry";
 
 const CountriesList = () => {
   const [countries, setCountries] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get('https://restcountries.com/v3.1/all')
-      .then(response => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
         setCountries(response.data);
       })
-      .catch(error => {
-        setError('Failed to fetch data');
+      .catch((error) => {
+        setError("Failed to fetch data");
       });
   }, []);
 
@@ -23,10 +25,13 @@ const CountriesList = () => {
   return (
     <div>
       <h1>Countries List</h1>
+      <SearchField onSearch={handleSearch} />
       <ul className="list-group">
-        {countries.map(country => (
+        {countries.map((country) => (
           <li key={country.cca3} className="list-group-item">
-            <Link to={`/country/${country.name.common}`}>{country.name.common}</Link>
+            <Link to={`/country/${country.name.common}`}>
+              {country.name.common}
+            </Link>
           </li>
         ))}
       </ul>
